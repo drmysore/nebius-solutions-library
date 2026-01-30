03f4647a286e9d5869d1a1b862a927e6399e67fc
Patrick Butler <pbutler@nebius.com>
2026-01-30 17:06:32 -0500

    feat: add a binpacking scheduler to k8s
    Added as a module to k8s-training

--- k8s-training/variables.tf
+++ k8s-training/variables.tf
@@ -347,3 +347,15 @@ variable "enable_opa_gatekeeper" {
   type        = bool
   default     = false
 }
+
+variable "enable_binpacking" {
+  description = "Enable using a Bin Packing Scheduler instead of round robin style scheduler"
+  type        = bool
+  default     = false
+}
+
+variable "force_binpacking_default" {
+  description = "If binpacking is enabled force it for the default namespace, instead of requiring each pod to opt-in"
+  type        = bool
+  default     = false
+}
